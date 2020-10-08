6ecb7f2f4520a5f5eca10bcaa385a86b49aac41d
luzhonglai <1342341673@qq.com>
2020-10-08 21:58:22 +0800

    init client mongodb

--- config.js
+++ config.js
@@ -5,9 +5,28 @@
  */
 
 module.exports = {
-  port: 8811, // 启动端口
-  db_URI: "mongodb://127.0.0.1:27017/forestry", //数据库地址
-  isHttp: true,
-  wechatAppid: "", //小程序appid
-  wechatSecret: "" //小程序
+  port: 8811, // 项目端口号 --- 启动端口
+  useHttp: true, // true http ---  false https 
+  wechat:{
+    wechatAppid: "", //小程序appid
+    wechatSecret: "", //小程序
+  },
+  dbConfig: {
+    port: 27017,
+    host: 'mongod://127.0.0.1:27017',
+    dbName: 'blog',
+  },
+  sessToken: {
+    host: '127.0.0.1',
+    dbName: 'blog',
+    collection: 'Token',
+    maxAge: 84600,
+  },
+  sesssCookie: {
+    propt: 27017,
+    host: '127.0.0.1',
+    dbName: 'blog',
+    collection: 'Cookie',
+    maxAge: 84600,
+  }
 };
